a8348bef08dfc6349ba780265cb31dd405410636
Thurston Lui <thurstonlui@gmail.com>
2020-08-20 03:59:48 +0000

    add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -10,7 +10,7 @@ if (cold) {
 }
 else{
   console.log('run wild');
-}*/
+}
 
 
 const temp = 99
@@ -23,4 +23,21 @@ else if (temp <15) {
 else 
   console.log('pajama time');
 
-  console.log('now you\'re ready, go'); 
+  console.log('now you\'re ready, go'); */
+
+  const isCitizen = true;
+  const age = 66
+
+  if (isCitizen && age>65) {
+    console.log('get that pension');
+  }
+temp = -200
+  if (temp <-40 || temp > 40) {
+    console.log("stay indoors all times!")
+  }
+
+raining = false;
+
+if (!raining) {
+  console.log('run like the wind');
+}
